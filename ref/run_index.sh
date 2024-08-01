6ec6a22694e2391bae696b4e683b8f7456695e21
kimzz14 <kimzz1004@gmail.com>
2024-08-01 14:49:43 +0900

    edit index

--- ref/run_index.sh
+++ ref/run_index.sh
@@ -1,3 +1,11 @@
-chromap -i -r ref.fa -o ref.index 1> run_chromap-index.log 2> run_chromap-index.err
+chromap \
+    -i \
+    -r ref.fa \
+    -o ref.index \
+    -w 13 \
+    1> run_chromap-index.log \
+    2> run_chromap-index.err
+
+#Wheat window size: 13
 
 samtools faidx ref.fa
